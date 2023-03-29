b3f1dd1ac0cd122a08adf3b86112a8082334a071
n2kkrd <65614635+n2kkrd@users.noreply.github.com>
2023-03-29 17:49:14 +0300

    .

--- vue2sportmaster/src/components/ArticleList.vue
+++ vue2sportmaster/src/components/ArticleList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
+    <div v-if="loading" class="loading">
+      Загрузка...
+      <button @click="cancelFetch">Отмена</button>
+    </div>
+    <div v-else>
     <router-link v-for="article in articles" :key="article.id" :to="'/article/' + article.id" tag="div">
       <article-block
         :title="article.title"
@@ -12,6 +17,7 @@
       ></article-block>
     </router-link>
     <ArticleForm @add-article="addArticle" />
+    </div>
   </div>
 </template>
 
@@ -26,18 +32,38 @@ export default {
     ArticleBlock,
     ArticleForm
   },
+  data(){
+    return{
+      loading:false,
+    }
+  },
   computed: {
-    ...mapState(['articles']),
+    ...mapState(['articles', 'loading']),
     ...mapGetters(['getArticleById']),
     articleCount() {
       return this.articles.length;
     },
   },
   methods: {
-    ...mapActions(['loadArticles', 'addArticle', 'togglePublished'])
+    ...mapActions(['fetchArticles', 'addArticle', 'togglePublished']),
+    async cancelFetch() {
+      if (this.$store.state.controller) {
+        this.$store.state.controller.abort();
+        this.$store.commit('SET_CONTROLLER', null);
+        this.$store.commit('SET_LOADING', false);
+      }
+    },
   },
-  beforeCreate() {
-    this.$store.dispatch('fetchArticles');
+  created() {
+    this.loading = true;
+    this.fetchArticles()
+      .then(() => {
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.error(error);
+        this.loading = false;
+      });
   }
 }
 </script>
--- vue2sportmaster/src/store/index.js
+++ vue2sportmaster/src/store/index.js
@@ -1,17 +1,24 @@
 import Vue from 'vue';
+import axios from 'axios';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const api = axios.create({
+  baseURL: 'http://localhost:10000',
+});
+
 export default new Vuex.Store({
   state: {
     articles: [],
+    loading: false,
+    controller: null,
   },
   mutations: {
     SET_ARTICLES(state, articles) {
       state.articles = articles;
     },
-    addArticle(state, article) {
+    ADD_ARTICLE(state, article) {
       state.articles.push(article);
     },
     TOGGLE_ARTICLE_PUBLISHED(state, articleId) {
@@ -20,20 +27,50 @@ export default new Vuex.Store({
         article.published = !article.published;
       }
     },
+    SET_LOADING(state, loading) {
+      state.loading = loading;
+    },
+    SET_CONTROLLER(state, controller) {
+      state.controller = controller;
+    },
   },
   actions: {
-    async fetchArticles({ commit }) {
+    async fetchArticles({ commit, state }) {
       try {
-        const response = await fetch('/articles.json');
-        const articles = await response.json();
+        if (state.controller) {
+          state.controller.abort();
+        }
+
+        const controller = new AbortController();
+        const signal = controller.signal;
+        commit('SET_CONTROLLER', controller);
+        commit('SET_LOADING', true);
+
+        const response = await api.get('/articles', { signal });
+        const articles = response.data;
+
         commit('SET_ARTICLES', articles);
+        commit('SET_LOADING', false);
       } catch (error) {
         console.error(error);
+        commit('SET_LOADING', false);
       }
     },
-    addArticle({commit},article){
-      commit('ADD_ARTICLE',article)
-    }
+
+    async addArticle({ commit }, article) {
+      try {
+        commit('SET_LOADING', true);
+
+        const response = await api.post('/articles', article);
+        const newArticle = response.data;
+
+        commit('ADD_ARTICLE', newArticle);
+        commit('SET_LOADING', false);
+      } catch (error) {
+        console.error(error);
+        commit('SET_LOADING', false);
+      }
+    },
   },
   getters: {
     publishedArticles(state) {
@@ -41,6 +78,9 @@ export default new Vuex.Store({
     },
     unpublishedArticles(state) {
       return state.articles.filter(a => !a.published);
+    },
+    isLoading(state) {
+      return state.loading;
     },
   },
 });
